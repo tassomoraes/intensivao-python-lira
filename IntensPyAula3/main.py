65030d63b359deb231174cc85a1a9b00c4360606
Tasso Moraes <tassoluis@gmail.com>
2021-07-05 14:47:44 -0300

    Analise exploratória dos dados

--- IntensPyAula3/main.py
+++ IntensPyAula3/main.py
@@ -4,3 +4,16 @@ import pandas as pd
 # Passo 1: extração dos dados
 tabela = pd.read_csv("advertising.csv")
 print(tabela)
+
+# Passo 2: Análise exploratória
+# bibliotecas para pltar gráficos
+import seaborn as sns
+import matplotlib.pyplot as plt # nescessário para usar o seaborn
+
+# mapa de calor para exibir as correlações da tabela
+sns.heatmap(tabela.corr(), annot=True, cmap="Wistia") # annot exibe valores nos quadrados; cmap muda as cores
+plt.show()
+
+sns.pairplot(tabela)
+plt.show()
+
