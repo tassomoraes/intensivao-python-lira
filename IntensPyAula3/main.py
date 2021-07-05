8bbc6b21a15fc2b54d99bae5bd58e998074a471a
Tasso Moraes <tassoluis@gmail.com>
2021-07-05 17:23:38 -0300

    Criando e trainando modelos

--- IntensPyAula3/main.py
+++ IntensPyAula3/main.py
@@ -26,3 +26,14 @@ y = tabela["Vendas"]
 x = tabela.drop("Vendas", axis=1)   # todas as colunas menos a conluna 'Vendas'
 
 x_treino, x_teste, y_treino, y_teste = train_test_split(x,y, test_size=0.3, radom_state=1)     # 30% da base de dados é para teste; radom_state sempre mesma forma radomica
+
+# já que o objetivo é número usa-se um método de regressão
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+
+#criando os modelos
+modelo_regressaoLinear = LinearRegression()
+modelo_arvoreDeDecisao = RandomForestRegressor()
+#treinando os modelos
+modelo_regressaoLinear.fit(x_treino,y_treino)
+modelo_arvoreDeDecisao.fit(x_treino,y_treino)
